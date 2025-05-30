ea3345cbbe193b1179c59b3c1a579f2edf8202cc
snoweager <158055689+snoweager@users.noreply.github.com>
2025-05-30 16:30:26 +0530

    Add files via upload

---
+++ seo-mobile/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WebView 
+        source={{ uri: 'http://192.168.29.56:3000' }} 
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
